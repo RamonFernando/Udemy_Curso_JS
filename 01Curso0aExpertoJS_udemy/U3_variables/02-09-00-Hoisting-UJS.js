f140d5c85f39772801d33ef6bd96cb77002c6fc3
RamonFernando <ramonrfpa88@gmail.com>
2024-11-02 18:24:08 +0100

    ¨todo¨

---
+++ 01Curso0aExpertoJS_udemy/U3_variables/02-09-00-Hoisting-UJS.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// Hoisting (podemos usar una variable, y despues declararla)
+//var x; // 1. Declarar la variable
+x = 10; // 2. Inicializamos la variable
+//y = 5; // al usar let no aplica el concepto de hoisting
+console.log(x);
+
+var x;
+let y;
---
+++ 01Curso0aExpertoJS_udemy/U4_operadoresAritmeticos/03-06-00-EjemploOperadoresComparacion-UJS.js
@@ -0,0 +1,17 @@
+// Operadores Relacionales (Comparacion)
+let a = 5;
+let b = '5';
+console.log(a);
+console.log(b);
+
+// Operadores igualdad ==
+// (solo compara valores, y hace una conversion si es necesario)
+console.log("a == b -> ", a == b);
+// String interpolation
+console.log(`${a} == ${b} -> ${a == b}`);
+
+// Operador igualdad estricta o exacto
+// se compara el valor y el tipo de dato
+console.log("a === b -> ", a === b);
+// String interpolation
+console.log(`${a} === ${b} -> ${a === b}`);
