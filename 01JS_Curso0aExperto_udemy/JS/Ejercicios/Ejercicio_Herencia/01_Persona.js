d625833ef2c8a3116c4184870db27c21165fb460
RamonFernando <ramonrfpa88@gmail.com>
2024-11-25 23:20:25 +0100

    clases curso JS

--- 01JS_Curso0aExperto_udemy/JS/Ejercicios/Ejercicio_Herencia/01_Persona.js
+++ 01JS_Curso0aExperto_udemy/JS/Ejercicios/Ejercicio_Herencia/01_Persona.js
@@ -1,43 +1,36 @@
-class Persona{
+class Persona {
 
     static contadorPersonas = 0;
-
-    constructor(nombre, apellido, edad){
+    constructor(nombre, apellido, edad) {
         this._idPersona = ++Persona.contadorPersonas;
         this._nombre = nombre;
         this._apellido = apellido;
         this._edad = edad;
     }
 
-    get idPersona(){
+    // Getters y Setters
+    get idPersona() {
         return this._idPersona;
     }
-
-    get nombre(){
+    get nombre() {
         return this._nombre;
     }
-
-    set nombre(nombre){
+    set nombre(nombre) {
         this._nombre = nombre;
-    }   
-
+    }
     get apellido(){
         return this._apellido;
     }
-
-    set apellido( apellido ){
+    set apellido(apellido){
         this._apellido = apellido;
     }
-
     get edad(){
         return this._edad;
     }
-
-    set edad( edad ){
+    set edad(edad){
         this._edad = edad;
     }
-
-    toString(){
-        return `${this._idPersona} ${this._nombre} ${this._apellido} ${this.edad}`; 
+    toString() {
+        return `${this._idPersona} ${this._nombre} ${this._apellido} ${this.edad}`;
     }
 }
